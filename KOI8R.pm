69e8484ea8e20b41141eadb46971cfc6a5ad54bc
INABA Hitoshi <ina@cpan.org>
2013-07-07 14:31:52 +0000

    Import of INA/Char-KOI8R-0.91 from CPAN.
    gitpan-cpan-distribution: Char-KOI8R
    gitpan-cpan-version:      0.91
    gitpan-cpan-path:         INA/Char/KOI8R/Char-KOI8R-0.91.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- KOI8R.pm
+++ KOI8R.pm
@@ -29,7 +29,7 @@ BEGIN {
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.90 $ =~ /(\d+)/oxmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.91 $ =~ /(\d+)/oxmsg;
 
 BEGIN { CORE::require Ekoi8r; }
 
@@ -489,7 +489,7 @@ sub KOI8R::escape_script {
         # in Chapter 5: Pattern Matching
         # of ISBN 978-0-596-00492-7 Programming Perl 4th Edition.
 
-        $e_script .= sprintf("use Ekoi8r %s;\n", $KOI8R::VERSION); # require run-time routines version
+        $e_script .= sprintf("use Ekoi8r %s.0;\n", $KOI8R::VERSION); # require run-time routines version
 
         # use KOI8R version qw(ord reverse getc);
         $function_ord     = 'ord';
@@ -5411,12 +5411,6 @@ KOI8R.pm removes 'bytes::' at head of function name.
   bytes::substr    substr    Byte
   ---------------------------------------
 
-=head1 Escaping Built-in Standard Module (Ekoi8r.pm provides)
-
-Ekoi8r.pm does "BEGIN { unshift @INC, '/Perl/site/lib/KOI8R' }" at head.
-Store the standard module modified for KOI8R software in this directory to
-override built-in standard modules.
-
 =head1 Escaping Standard Module Content (You do)
 
 You need copy built-in standard module to /Perl/site/lib/KOI8R and change
@@ -6052,3 +6046,4 @@ I am thankful to all persons.
 
 =cut
 
+# Did you fun enough? ^o^)
